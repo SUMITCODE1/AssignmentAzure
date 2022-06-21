1d6e8b4be3b47cf20c18f22cc0640f19a1124973
Sumit <sumitsinghindia5@gmail.com>
2022-06-21 13:25:44 +0530

    sumit Assignment

---
+++ Assignment1Sumit/Assignment1Sumit/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Assignment1Sumit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int fatherAge = 40;
+
+            int SonAge = 22;
+
+            int Diff=fatherAge-SonAge;
+
+            string X = "surya";
+
+            string Y = "arvind";
+            X = X.ToUpper();
+            Y = Y.ToUpper();
+
+            //Convert father and son names as uppercase
+
+            Console.WriteLine( $"Father name is {X} And his present age is { fatherAge } ");
+
+            Console.WriteLine( $"Son name is { Y } And his present age is { SonAge } ");
+
+            Console.WriteLine( $"What is the difference between father and son: { Diff}");
+
+            Console.Read();
+
+        }
+    }
+}
